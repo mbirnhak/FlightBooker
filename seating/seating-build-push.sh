4f00e2f2d1fed2defed59d56e9b694982a459315
Matthew Birnhak <MattBirnhak@10-252-181-103.student.trincoll.edu>
2023-04-29 13:48:12 -0400

    bash shell updated commit

--- seating/seating-build-push.sh
+++ seating/seating-build-push.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 VAR="${1:-mbirnhak}"
 
-docker context create buildx-build
-docker buildx create --use buildx-build
 chmod +x gradlew
 ./gradlew build 
-docker buildx build --push --platform linux/amd64, linux/arm64 --tag "$VAR"/seating:v1 .
+docker buildx build --push --platform linux/amd64,linux/arm64 --tag "$VAR"/seating:v1 .
