13c191ddf8a21c4422fc8b4aea664ab3f63f0bd2
Matthew Birnhak <MattBirnhak@10-252-181-103.student.trincoll.edu>
2023-05-08 20:12:46 -0400

    yamls updated for k8

--- cluster/apply-yaml.sh
+++ cluster/apply-yaml.sh
@@ -1,4 +1,6 @@
 pushd ../cluster
 export APP_USER="${1:-bsimoes2001}"
 sed "s/APP_USER/$1/g" mongo-deployment.yaml | kubectl apply -f -
+kubectl wait deployment mongo --for condition=Available=True
 for f in *.yaml; do envsubst < $f | kubectl apply -f -; done
+popd
