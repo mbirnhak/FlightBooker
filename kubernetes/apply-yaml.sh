4da27594d010612991ce6bc4f85db5414f355834
Matthew Birnhak <MattBirnhak@MacBook-Pro-44.local>
2023-05-13 11:48:14 -0400

    bash shell change

--- kubernetes/apply-yaml.sh
+++ kubernetes/apply-yaml.sh
@@ -1,6 +1,5 @@
-pushd ../cluster
+pushd ../kubernetes
 export APP_USER="${1:-bsimoes2001}"
 sed "s/APP_USER/$1/g" mongo-deployment.yaml | kubectl apply -f -
 kubectl wait deployment mongo --for condition=Available=True --timeout=300s
 for f in *.yaml; do envsubst < $f | kubectl apply -f -; done
-popd
